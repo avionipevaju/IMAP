85ac99180a73a35510b67df19cea287779290e3a
Pavle10 <pavlemark@hotmail.rs>
2016-05-16 21:34:11 +0200

    - Added new responds

--- src/Server.java
+++ src/Server.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 public class Server {
 	
 	private ArrayList<Mailbox> mMailboxes;
+	private Mailbox mCurrentMailbox = null;
 	private ServerSocket mServerSocket;
 	private Socket mSocket;
 	private BufferedReader mInput;
@@ -23,7 +24,8 @@ public class Server {
 	private State mCurrentState;
 	
 	public Server() throws Exception {
-		mMailboxes = new ArrayList<>();
+		initMailboxes();
+		
 		mServerSocket = new ServerSocket(2015); //143
 		mSocket = mServerSocket.accept();
 		
@@ -31,9 +33,6 @@ public class Server {
 		mOutput = new PrintWriter(new OutputStreamWriter(mSocket.getOutputStream()),true);
 		
 		mCurrentState = State.Not_Authenticated;
-		
-		//Not Authenticated State
-		//AUTHENTICATE, LOGIN, STARTTLS valid commands
 		
 		mMessage = mConst.concat(mGreeting);
 		System.out.println(mMessage);
@@ -62,6 +61,20 @@ public class Server {
 		
 		mSocket.close();
 		
+	}
+	
+	private void initMailboxes() {
+		mMailboxes = new ArrayList<>();
+		
+		Mailbox inbox = new Mailbox("Inbox");
+		Mailbox sent = new Mailbox("Sent");
+		Mailbox drafts = new Mailbox("Drafts");
+		Mailbox trash = new Mailbox("Trash");
+		
+		mMailboxes.add(inbox);
+		mMailboxes.add(sent);
+		mMailboxes.add(drafts);
+		mMailboxes.add(trash);
 	}
 	
 	public void parseCommand() {
@@ -134,7 +147,7 @@ public class Server {
 
 		case "AUTHENTICATE" :
 			if (mCurrentState != State.Not_Authenticated) {
-				mMessage = mConst.concat(mTag.concat(" BAD unknown command"));
+				mMessage = mConst.concat(mTag.concat(" BAD AUTHENTICATE unknown command"));
 				System.out.println(mMessage);
 				mOutput.println(mMessage);
 				return ;
@@ -168,7 +181,7 @@ public class Server {
 			
 		case "LOGIN" :
 			if (mCurrentState != State.Not_Authenticated) {
-				mMessage = mConst.concat(mTag.concat(" BAD unknown command"));
+				mMessage = mConst.concat(mTag.concat(" BAD LOGIN unknown command"));
 				System.out.println(mMessage);
 				mOutput.println(mMessage);
 				return ;
@@ -189,25 +202,158 @@ public class Server {
 
 		case "SELECT" :
 			if (mCurrentState != State.Authenticated) {
-				mMessage = mConst.concat(mTag.concat("BAD unknown command"));
+				mMessage = mConst.concat(mTag.concat("BAD SELECT unknown command"));
+				System.out.println(mMessage);
+				mOutput.println(mMessage);
+				return ;
+			}
+			if (!checkMailbox(mData)){
+				mMessage = mConst.concat(mTag.concat(" NO SELECT no such mailbox or can't access"));
 				System.out.println(mMessage);
 				mOutput.println(mMessage);
 				return ;
 			}
 			
+			int counter = mCurrentMailbox.exists();
+			mMessage = mConst.concat("* " + counter + " EXISTS");
+			System.out.println(mMessage);
+			mOutput.println(mMessage);
+			
+			counter = mCurrentMailbox.recent();
+			mMessage = mConst.concat("* " + counter + " RECENT");
+			System.out.println(mMessage);
+			mOutput.println(mMessage);
+			
+			counter = mCurrentMailbox.unseen();
+			mMessage = mConst.concat("* OK [UNSEEN " + counter + "] Message " + counter + " is first unseen");
+			System.out.println(mMessage);
+			mOutput.println(mMessage);
+			
+			mMessage = mConst.concat("* FLAGS " + mCurrentMailbox.toStringDefinedFlags());
+			System.out.println(mMessage);
+			mOutput.println(mMessage);
+
+			mMessage = mConst.concat("* OK [PERMANENTFLAGS " + mCurrentMailbox.toStringPermanentFlags() + "]");
+			System.out.println(mMessage);
+			mOutput.println(mMessage);
+			
+			mMessage = mConst.concat(mTag.concat(" OK [READ-WRITE] SELECT completed"));
+			mCurrentState = State.Selected;
+			System.out.println(mMessage);
+			mOutput.println(mMessage);
 
 			break;
 			
 		case "EXAMINE" :
+			if (mCurrentState != State.Authenticated) {
+				mMessage = mConst.concat(mTag.concat("BAD EXAMINE unknown command"));
+				System.out.println(mMessage);
+				mOutput.println(mMessage);
+				return ;
+			}
+			if (!checkMailbox(mData)){
+				mMessage = mConst.concat(mTag.concat(" NO EXAMINE no such mailbox or can't access"));
+				System.out.println(mMessage);
+				mOutput.println(mMessage);
+				return ;
+			}
+			
+			int examineCounter = mCurrentMailbox.exists();
+			mMessage = mConst.concat("* " + examineCounter + " EXISTS");
+			System.out.println(mMessage);
+			mOutput.println(mMessage);
+			
+			examineCounter = mCurrentMailbox.recent();
+			mMessage = mConst.concat("* " + examineCounter + " RECENT");
+			System.out.println(mMessage);
+			mOutput.println(mMessage);
+			
+			examineCounter = mCurrentMailbox.unseen();
+			mMessage = mConst.concat("* OK [UNSEEN " + examineCounter + "] Message " + examineCounter + " is first unseen");
+			System.out.println(mMessage);
+			mOutput.println(mMessage);
+			
+			mMessage = mConst.concat("* FLAGS " + mCurrentMailbox.toStringDefinedFlags());
+			System.out.println(mMessage);
+			mOutput.println(mMessage);
+
+			mMessage = mConst.concat("* OK [PERMANENTFLAGS ()] No permanent flags permitted");
+			System.out.println(mMessage);
+			mOutput.println(mMessage);
+			
+			mMessage = mConst.concat(mTag.concat(" OK [READ-ONLY] EXAMINE completed"));
+			mCurrentState = State.Selected;
+			System.out.println(mMessage);
+			mOutput.println(mMessage);
+
 			break;
 			
 		case "CREATE" :
+			if (mCurrentState != State.Authenticated) {
+				mMessage = mConst.concat(mTag.concat("BAD CREATE unknown command"));
+				System.out.println(mMessage);
+				mOutput.println(mMessage);
+				return ;
+			}
+			if (checkMailbox(mData)){
+				mMessage = mConst.concat(mTag.concat(" NO CREATE can't create mailbox with that name"));
+				System.out.println(mMessage);
+				mOutput.println(mMessage);
+				return ;
+			}
+			
+			String name = mData.trim();
+			Mailbox newMailbox = new Mailbox(name);
+			mMailboxes.add(newMailbox);
+			mMessage = mConst.concat(mTag.concat("OK CREATE completed"));
+			System.out.println(mMessage);
+			mOutput.println(mMessage);
+
 			break;
 			
 		case "DELETE" :
+			if (mCurrentState != State.Authenticated) {
+				mMessage = mConst.concat(mTag.concat("BAD DELETE unknown command"));
+				System.out.println(mMessage);
+				mOutput.println(mMessage);
+				return ;
+			}
+			if (!checkMailbox(mData)){
+				mMessage = mConst.concat(mTag.concat(" NO DELETE can't delete mailbox with that name"));
+				System.out.println(mMessage);
+				mOutput.println(mMessage);
+				return ;
+			}
+
+			mMailboxes.remove(mCurrentMailbox);
+			mCurrentMailbox = null;
+			
+			mMessage = mConst.concat(mTag.concat("OK DELETE completed"));
+			System.out.println(mMessage);
+			mOutput.println(mMessage);
+
 			break;
 			
 		case "RENAME" :
+			if (mCurrentState != State.Authenticated) {
+				mMessage = mConst.concat(mTag.concat("BAD RENAME unknown command"));
+				System.out.println(mMessage);
+				mOutput.println(mMessage);
+				return ;
+			}
+			String newName = getNewName();
+			if (!checkMailbox(mData)){
+				mMessage = mConst.concat(mTag.concat(" NO RENAME rename failure"));
+				System.out.println(mMessage);
+				mOutput.println(mMessage);
+				return ;
+			}
+			
+			mCurrentMailbox.setName(newName.toLowerCase());
+			mMessage = mConst.concat(mTag.concat("OK RENAME completed"));
+			System.out.println(mMessage);
+			mOutput.println(mMessage);
+
 			break;
 			
 		case "SUBSCRIBE" :
@@ -265,13 +411,34 @@ public class Server {
 		Scanner temp = new Scanner(data);
 		username = temp.next();
 		password = temp.next();
-		System.out.println(username);
-		System.out.println(password);
+		//System.out.println(username);
+		//System.out.println(password);
 		
 		if (mUsername.equalsIgnoreCase(username) && mPassword.equalsIgnoreCase(password))
 			return true;
 
 		return false;
+	}
+	
+	private boolean checkMailbox(String mData) {
+		for (Mailbox m : mMailboxes) {
+			if (mData.trim().equalsIgnoreCase(m.getName()))
+				mCurrentMailbox = m;
+				return true;
+		}
+		return false;
+	}
+	
+	private String getNewName() {
+		Scanner temp = new Scanner(mData);
+		
+		String toRename = temp.next();
+		String result = temp.next();
+		System.out.println(toRename);
+		System.out.println(result);
+		
+		mData = toRename;
+		return result;
 	}
 
 
