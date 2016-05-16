997ca280cf8883735a49240d797d15a636f2a8f7
Nikola <antic.nikola@outlook.com>
2016-05-16 15:04:31 +0200

    - Initial client implementation

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,18 @@
 
 public class Client {
 
+	public Client() throws Exception {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+
+		try {
+			new Client();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
