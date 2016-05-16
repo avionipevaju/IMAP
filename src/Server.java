169bc644b55ef54c88e91beb47bf0a20051d7e72
Pavle10 <pavlemark@hotmail.rs>
2016-05-16 15:00:52 +0200

    - Implemented Mailbox and Message classes

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,17 @@
 
 public class Server {
+	
+	public Server() throws Exception {
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		try {
+			new Server();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
