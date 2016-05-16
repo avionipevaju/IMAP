b085453eeb6c5deff906a88c6fc2c597f83b5244
Pavle10 <pavlemark@hotmail.rs>
2016-05-16 17:40:09 +0200

    - Updated Server

--- src/Server.java
+++ src/Server.java
@@ -15,13 +15,14 @@ public class Server {
 	private PrintWriter mOutput;
 	private final String mGreeting = "*OK IMAP Service Ready";
 	private final String mConst = "S: ";
-	private String mCommand, mMessage;
+	private String mMessage, mCommand, mTag, mData;
+	private int mNOOPCount = 1;
 	
 	public Server() throws Exception {
 		mMailboxes = new ArrayList<>();
 		mServerSocket = new ServerSocket(2015); //143
 		mSocket = mServerSocket.accept();
-		System.out.println("Address" + mSocket.getInetAddress());
+		
 		mInput = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
 		mOutput = new PrintWriter(new OutputStreamWriter(mSocket.getOutputStream()),true);
 		
@@ -32,18 +33,46 @@ public class Server {
 		System.out.println(mMessage);
 		
 		mOutput.println(mMessage);
-		mCommand = mInput.readLine();
+		mMessage = mInput.readLine();
+		System.out.println(mMessage);
 		
-		System.out.println(mCommand);
+		parseCommand();
 		
+		while (!mCommand.equalsIgnoreCase("LOGOUT")); {
+			doCommand();
+			
+			mMessage = mInput.readLine();
+			System.out.println(mMessage);
+			parseCommand();
+		}
 		
+		mMessage = mConst.concat("* BYE IMAP server terminating connection");
+		System.out.println(mMessage);
+		mOutput.println(mMessage);
 		
-		
+		mMessage = mConst.concat(mTag.concat(" OK LOGOUT completed"));
+		System.out.println(mMessage);
+		mOutput.println(mMessage);
 		
 		mSocket.close();
 		
 	}
 	
+	public void parseCommand() {
+		int i = 8, startCommand = 8;
+		mTag = mMessage.substring(3, 7);
+		while (i < mMessage.length() && mMessage.charAt(i) != ' ') {
+			i++;
+		}
+		mCommand = mMessage.substring(startCommand, i);
+		if (i != mMessage.length())
+			mData = mMessage.substring(i);
+		else {
+			mData = "";
+		}
+		
+	}
+		
 	
 	public static void main(String[] args) {
 		try {
@@ -52,5 +81,122 @@ public class Server {
 			e.printStackTrace();
 		}
 	}
+	
+	public void doCommand() {
+		
+		switch(mCommand) {
+		
+		case "CAPABILITY" :
+			
+			mMessage = mConst.concat("* CAPABILITY IMAP AUTH=PLAIN");
+			System.out.println(mMessage);
+			mOutput.println(mMessage);
+			
+			mMessage = mConst.concat(mTag.concat(" OK CAPABILITY completed"));
+			System.out.println(mMessage);
+			mOutput.println(mMessage);
+			
+			break;
+			
+		case "NOOP" :
+			
+			if (mNOOPCount == 3) {
+				//TODO kad se implementira mailbox srediti
+				
+				mNOOPCount = 1;
+			}else {
+				mMessage = mConst.concat(mTag.concat(" OK NOOP completed"));
+				System.out.println(mMessage);
+				mOutput.println(mMessage);
+				mNOOPCount++;
+			}
+			
+			break;
+		
+		case "LOGOUT" :
+			
+			mMessage = mConst.concat("* BYE IMAP Server logging out");
+			System.out.println(mMessage);
+			mOutput.println(mMessage);
+			
+			mMessage = mConst.concat(mTag.concat(" OK LOGOUT completed"));
+			System.out.println(mMessage);
+			mOutput.println(mMessage);
+			
+			break;
+
+		case "STARTTLS" :
+			break;
+
+		case "AUTHENTICATE" :
+			break;
+			
+		case "LOGIN" :
+			break;
+
+		case "SELECT" :
+			break;
+			
+		case "EXAMINE" :
+			break;
+			
+		case "CREATE" :
+			break;
+			
+		case "DELETE" :
+			break;
+			
+		case "RENAME" :
+			break;
+			
+		case "SUBSCRIBE" :
+			break;
+			
+		case "UNSUBSCRIBE" :
+			break;
+			
+		case "LIST" :
+			break;
+			
+		case "LSUB" :
+			break;
+			
+		case "STATUS" :
+			break;
+
+		case "APPEND" :
+			break;
+			
+		case "CHECK" :
+			break;
+			
+		case "CLOSE" :
+			break;
+			
+		case "EXPUNGE" :
+			break;
+			
+		case "SEARCH" :
+			break;
+
+		case "FETCH" :
+			break;
+
+		case "STORE" :
+			break;
+
+		case "COPY" :
+			break;
+
+		case "UID" :
+			break;
+
+		default :
+			break;
+		
+		}
+		
+	}
+
 
 }
