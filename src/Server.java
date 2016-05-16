e0580cb3a125b69fe2137d085d225f28d1fcf59f
Pavle10 <pavlemark@hotmail.rs>
2016-05-16 15:56:17 +0200

    - Changed Server

--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server {
 	
+	private ArrayList<Mailbox> mMailboxes;
+	private ServerSocket mServerSocket;
+	private Socket mSocket;
+	private BufferedReader mInput;
+	private PrintWriter mOutput;
+	private final String mGreeting = "*OK IMAP Service Ready";
+	private final String mConst = "S: ";
+	private String mCommand, mMessage;
+	
 	public Server() throws Exception {
+		mMailboxes = new ArrayList<>();
+		mServerSocket = new ServerSocket(2015); //143
+		mSocket = mServerSocket.accept();
+		System.out.println("Address" + mSocket.getInetAddress());
+		mInput = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
+		mOutput = new PrintWriter(new OutputStreamWriter(mSocket.getOutputStream()),true);
+		
+		//Not Authenticated State
+		//AUTHENTICATE, LOGIN, STARTTLS valid commands
+		
+		mMessage = mConst.concat(mGreeting);
+		System.out.println(mMessage);
+		
+		mOutput.println(mMessage);
+		mCommand = mInput.readLine();
+		
+		System.out.println(mCommand);
+		
+		
+		
+		
+		
+		mSocket.close();
 		
 	}
 	
